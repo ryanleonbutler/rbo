0525d69e007e7f096c75be16e7af07105baed520
ryanleonbutler <ryanleonbutler@gmail.com>
2020-12-17 07:36:53 +0200

    Added absolute path in deployment scripts

--- app_install.sh
+++ app_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/home/ec2-user/rbo/venv/bin/python manage.py migrate
-/home/ec2-user/rbo/venv/bin/python manage.py makemigrations blog
-/home/ec2-user/rbo/venv/bin/python manage.py collectstatic
+/home/ec2-user/rbo/venv/bin/python /home/ec2-user/rbo/manage.py migrate
+/home/ec2-user/rbo/venv/bin/python /home/ec2-user/rbo/manage.py makemigrations blog
+/home/ec2-user/rbo/venv/bin/python /home/ec2-user/rbo/manage.py collectstatic
