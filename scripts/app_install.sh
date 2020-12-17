5f931f5ad4588f0641883e9d5562a5f045cc63a6
ryanleonbutler <ryanleonbutler@gmail.com>
2020-12-17 09:33:30 +0200

    Added automated backups and backups to S3. Favicon on Resume page.

--- scripts/app_install.sh
+++ scripts/app_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+/home/ec2-user/rbo/venv/bin/pip install -r /home/ec2-user/rbo/requirements.txt
 /home/ec2-user/rbo/venv/bin/python /home/ec2-user/rbo/manage.py migrate
 /home/ec2-user/rbo/venv/bin/python /home/ec2-user/rbo/manage.py makemigrations blog
 /home/ec2-user/rbo/venv/bin/python /home/ec2-user/rbo/manage.py collectstatic --noinput
