b9e129cdaf5adc42b9f2bcd9ddecb70503a543fa
ryanleonbutler <ryanleonbutler@gmail.com>
2020-12-27 14:39:22 +0200

    Added slugs and human readible URL's

--- blog/views.py
+++ blog/views.py
@@ -1,50 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from blog.models import Post
 
-# TODO: Comments
-# from .forms import CommentForm
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = "post_list"
+    template_name = "posts.html"
 
 
-def posts(request):
-    posts = Post.objects.all().order_by('-created_on')
-    context = {
-        "posts": posts,
-    }
-    return render(request, "posts.html", context)
-
-
-def categories(request, category):
-    posts = Post.objects.filter(
-        categories__name__contains=category
-    ).order_by(
-        '-created_on'
-    )
-    context = {
-        "category": category,
-        "posts": posts
-    }
-    return render(request, "categories.html", context)
-
-
-def post(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    # TODO: Comments
-    # form = CommentForm()
-    # if request.method == 'POST':
-    #     form = CommentForm(request.POST)
-    #     if form.is_valid():
-    #         comment = Comment(
-    #             author=form.cleaned_data["author"],
-    #             body=form.cleaned_data["body"],
-    #             post=post
-    #         )
-    #         comment.save()
-
-    context = {
-        "post": post,
-        # "comments": comments,  # TODO: Comments
-        # "form": form, # TODO: Comments
-    }
-
-    return render(request, "post.html", context)
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = "post_detail"
+    template_name = "post.html"
