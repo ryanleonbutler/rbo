40c7ba394bb0e369baf5c6fff8ff0d2f74613dae
ryanleonbutler <ryanleonbutler@gmail.com>
2020-12-29 06:55:29 +0200

    #10 closed

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from blog.views import PostDetailView, PostListView
+from . import views
 
 urlpatterns = [
     path("", PostListView.as_view(), name="posts"),
     path("<slug:slug>", PostDetailView.as_view(), name="post"),
+    path("<category>/", views.blog_category, name="blog_category"),
 ]
