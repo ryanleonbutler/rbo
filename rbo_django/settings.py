eb51e42844778ffa697bf579e4a48323f34bcaf5
ryanleonbutler <ryanleonbutler@gmail.com>
2020-12-29 22:20:40 +0200

    #11 close

--- rbo_django/settings.py
+++ rbo_django/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'rbo_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'ryan',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
