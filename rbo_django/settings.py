49d77e9205a025d5c8e12ebac9b97857e3f6aaf0
ryanleonbutler <ryanleonbutler@gmail.com>
2021-01-03 15:35:13 +0200

    #1

--- rbo_django/settings.py
+++ rbo_django/settings.py
@@ -50,10 +50,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
+
     'markdownx',
     'blog',
     'resume',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
