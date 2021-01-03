5fb9398b8b3edb2bf3ce328c67429abf73322eac
ryanleonbutler <ryanleonbutler@gmail.com>
2021-01-04 00:12:50 +0200

    #20 #6

--- rbo_django/settings.py
+++ rbo_django/settings.py
@@ -64,11 +64,12 @@ INSTALLED_APPS = [
     "django_user_agents",
     "tracking_analyzer",
     "markdownx",
+    "hitcount",
     "blog",
     "resume",
 ]
 
-SITE_ID = 1
+SITE_ID = 2
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -141,7 +142,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en-uk"
 
 TIME_ZONE = "UTC"
 
