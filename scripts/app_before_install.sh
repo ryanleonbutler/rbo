5b2995d2834f28edfe9998e29e3a9e289a17a787
ryanleonbutler <ryanleonbutler@gmail.com>
2021-03-25 14:19:09 +0200

    Fixed issue in pipeline script

--- scripts/app_before_install.sh
+++ scripts/app_before_install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-BACKUP_FILE="/home/ec2-user/rbo/db.sqlite3"
-mkdir /home/ec2-user/rbo_backup
-chown ec2-user -R /home/ec2-user/rbo_backup
+BACKUP_FILE="blog.dump"
 BACKUP_DIR="/home/ec2-user/rbo_backup"
+mkdir -p "$BACKUP_DIR"
+chown ec2-user -R "$BACKUP_DIR"
+
+# Backup
+mysqldump -u root blog > "$BACKUP_DIR"/"$BACKUP_FILE"
 
 # Delete backups older than 30 days
-if find "$BACKUP_FILE" -type f -mtime -31 | grep -q .
+if find "$BACKUP_DIR" -name "$BACKUP_FILE" -type f -mtime -31 | grep -q .
 then
         find "$BACKUP_DIR" -type f -mtime +30 -delete
 fi
@@ -15,5 +18,5 @@ last_backup=$(ls -t "$BACKUP_DIR" | head -n 1)
 if [ ! "$last_backup" ] || ! cmp -s "$BACKUP_FILE" "$BACKUP_DIR/$last_backup"
 then
         today=`date "+%Y-%m-%d_%H:%M:%S"`
-        cp "$BACKUP_FILE" "$BACKUP_DIR/$today.sqlite3"
+        cp "$BACKUP_FILE" "$BACKUP_DIR/$today.dump"
 fi
