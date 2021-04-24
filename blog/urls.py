fce995b6d19aaa3d437a7ff72164672d79954e59
ryanleonbutler <ryanleonbutler@gmail.com>
2021-04-25 00:40:41 +0200

    new theme

--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,18 @@
 from django.conf.urls import url
 from django.urls import path
-from blog.views import PostDetailView, PostListView
+from blog.views import PostDetailView, IndexListView, BlogListView
 from django.views.generic import TemplateView
 from . import views
 
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="posts"),
-    path('search/', views.post_search, name='search_results'),
-    url("contact/", views.page_contact, name="contact"),
-    url("about/", views.page_about, name="page"),
+    path("", IndexListView.as_view(), name="posts"),
+    path('search', views.post_search, name='search_results'),
     path("<slug:slug>", PostDetailView.as_view(), name="post"),
-    path("<category>/", views.post_category, name="post_category"),
+    path("<category>", views.post_category, name="post_category"),
+    url("blog", BlogListView.as_view(), name="posts"),
+    url("contact", views.page_contact, name="contact"),
+    url("about", views.page_about, name="page"),
     url("yandex_fd81df80c0db7580.html", views.yandex, name="yandex"),
     url("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type='text/plain'))
 ]
