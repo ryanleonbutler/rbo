e674160cbc9e1b058235dced05e2ea89e6b767f3
ryanleonbutler <ryanleonbutler@gmail.com>
2021-04-28 21:05:27 +0200

    Bug fix in blog post views

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.urls import path
-from blog.views import IndexListView, BlogListView
+from blog.views import IndexListView, BlogListView, NibbleListView
 from django.views.generic import TemplateView
 from . import views
 
@@ -17,15 +17,15 @@ sitemaps = {
 urlpatterns = [
     url("yandex_fd81df80c0db7580.html", views.yandex, name="yandex"),
     url("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
-    url("blog/", BlogListView.as_view(), name="posts"),
     url("contact/", views.page_contact, name="contact"),
     url("about/", views.page_about, name="about"),
     url('search/', views.post_search, name='search_results'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    # path("<slug:slug>", PostDetailView.as_view(), name="post"),
     path("blog/<slug:slug>", views.post_content, name="post"),
+    path("blog/", BlogListView.as_view(), name="posts"),
     path("blog/<category>", views.post_category, name="post_category"),
     path("nibbles/<slug:slug>", views.nibble_content, name="nibble"),
+    path("nibbles/", NibbleListView.as_view(), name="nibbles"),
     path("", IndexListView.as_view(), name="home"),
 ]
 
