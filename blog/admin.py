f18128a5474403a61d5d5023fad68be915ee3a28
ryanleonbutler <ryanleonbutler@gmail.com>
2021-07-10 14:47:34 +0200

    Added Nibble Categories to Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.db import models
-from markdownx.widgets import AdminMarkdownxWidget
 
-from blog.models import Category, Nibble, Post
+from blog.models import Category, Nibble, Post, NibbleCategory
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -13,20 +12,15 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "status", "created_on", "last_modified")
     list_filter = ("status",)
     search_fields = ["title"]
-    formfield_overrides = {
-        models.TextField: {"widget": AdminMarkdownxWidget},
-    }
 
 
 class NibbleAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "status", "created_on", "last_modified")
     list_filter = ("status",)
     search_fields = ["title"]
-    formfield_overrides = {
-        models.TextField: {"widget": AdminMarkdownxWidget},
-    }
 
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Nibble, NibbleAdmin)
+admin.site.register(NibbleCategory, NibbleAdmin)
