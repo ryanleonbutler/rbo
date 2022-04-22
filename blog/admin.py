4e807eb283432c59376aa4e06de9bc4314802ea7
Ryan <ryanleonbutler@gmail.com>
2022-04-22 12:20:40 +0200

    sitemap fixes

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.sites.models import Site
 from django.db import models
 
 from blog.models import Post, Tag
@@ -16,6 +17,20 @@ class PostAdmin(admin.ModelAdmin):
 
 class NibbleAdmin(admin.ModelAdmin):
     pass
+
+
+admin.site.unregister(Site)
+
+
+class SiteAdmin(admin.ModelAdmin):
+    fields = ("id", "name", "domain")
+    readonly_fields = ("id",)
+    list_display = ("id", "name", "domain")
+    list_display_links = ("name",)
+    search_fields = ("name", "domain")
+
+
+admin.site.register(Site, SiteAdmin)
 
 
 admin.site.register(Post, PostAdmin)
